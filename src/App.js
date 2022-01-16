9915b7fd1951de1d70846f78dd174ad3cfff9f56
rikoandre <97824888+rikoandre@users.noreply.github.com>
2022-01-16 14:54:19 +0700

    belajar ngoding

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import "./Assets/App.scss";
+function App() {
+  return (
+    <div className="container">
+      <div className="navbar">Header</div>
+
+      <div className="section">
+        <div className="Card part-1">1</div>
+        <div className="Card part-2">2</div>
+        <div className="Card part-3">3</div>
+        <div className="Card part-4">4</div>
+        <div className="Card part-5">5</div>
+        <div className="Card part-6">6</div>
+      </div>
+
+      <div className="body">
+        <div className= "body anak-1">1</div>
+        <div className= "body anak-2">2</div>
+        <div className= "body anak-3">3</div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
